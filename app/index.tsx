cd5ced03e6fb9c1d4031a966c98333aac3e036ed
Juel-Vargas <juelvargasrojas@gmail.com>
2025-07-31 16:48:38 -0400

    add: folder structure

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>Test text</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    color: "#555",
+  },
+
+});
---
+++ screens/CountdowScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const CountdowScreen = () => {
+  return (
+    <View>
+      <Text>CountdowScreen</Text>
+    </View>
+  )
+}
+
+export default CountdowScreen
